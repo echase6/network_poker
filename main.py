a92134224e84cfb422bfbc3ffe50711d77bc803c
Eric Chase <echase6@gmail.com>
2016-07-28 16:21:51 -0700

    main now exercises deck and hand generation

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-from card import Card
-from hand import Hand
+from card import Card, SUITS, RANKS
+from hand import Hand, generate_hand
 from stash import Stash
 from pot import Pot
 from player import Player
 from table import Table
-from deck import Deck
-
+from deck import Deck, generate_deck, shuffle_the_deck
 
 
 def main():
+    deck = generate_deck(SUITS, RANKS)
+    shuffle_the_deck(deck)
     t = Table()
     a = Player('Eric')
+    a.hand = generate_hand(deck)
     b = Player('Stephen')
+    b.hand = generate_hand(deck)
     t.players = [a, b]
     print(str(t))
 
