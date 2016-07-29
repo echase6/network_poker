6e37018c17dbb79a12a371424923610cb170c5b4
Eric Chase <echase6@gmail.com>
2016-07-29 16:12:10 -0700

    added docstrings and table rendering

--- modules/client.py
+++ modules/client.py
@@ -1,16 +1,23 @@
 """Client module manages connection to server.
 
-Hard-coded to work with a fixed port.
+Hard-coded to work with a fixed address and port, entered at startup.
+
+connect_to_server() connects the client to the server
+message_from_server() displays a message from the server
+answer_to_server() inquires the user and sends the response to the server
+render_table() unpacks the shipped JSON version of the table and displays it
+process_server_message() interprets the incoming info and acts appropriately
 """
 
 
 import socket
+import jsonpickle
 
 PORTS = [8000, 8001]
 
 
 def connect_to_server():
-    """Module to connect to the server..
+    """Module to connect the client to the server.
 
     Returns individual serversocket for communication.
     """
@@ -19,7 +26,7 @@ def connect_to_server():
     port = int(input('Port number: '))
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.connect((host, port))
-    return server
+    return server, port
 
 
 def message_from_server(s):
@@ -28,18 +35,53 @@ def message_from_server(s):
 
 
 def answer_to_server(msg_string, server):
-    """Retrieve answer from client.
-
-    Returns string that client entered.
-    """
+    """Sends answer to server."""
     server.send(msg_string.encode())
 
 
+def render_table(table_json):
+    """Render the table from received json file.
+
+    This shares code from the previously-written Table class __str__()
+    function, so this should be put in a separate module to be common to both.
+    """
+    table = jsonpickle.decode(table_json)
+    out_string_list = []
+    for player in table.players:
+        out_string = ''
+        out_string += 'Name: {:10s}  Hand: X-X '.format(player.name)
+        for card in player.hand.hand_list[1:]:
+            out_string += '{}-{} '.format(card.rank, card.suit).ljust(20)
+        out_string += 'Stash: {}   Status: {}'.format(player.stash.value,
+                                                      player.status)
+        out_string_list += [out_string]
+    pot_string = ' '*10 + 'Pot: {}'.format(table.pot.value)
+    return '\n\n'.join([out_string_list[0], pot_string, out_string_list[1]])
+
+
+def process_server_message(msg, server):
+    """Analyzes and acts on the content of a message from the server.
+
+    Valid actions are:
+      rendering a table (Table JSON was sent)
+      asking a question and sending response to server (msg ends with a ?)
+      displaying a message
+    """
+    if msg[1:6] == 'Table':  # Don't expect this to work just yet...
+        render_table(msg)
+    elif msg[-1] == '?':
+        answer = input(msg)
+        answer_to_server(answer, server)
+    else:
+        print(msg)
+    return
+
+
 def main():
-    socket = connect_to_server()
-    print(message_from_server(socket))
-    answer = input()
-    answer_to_server(answer, socket)
+    socket, port = connect_to_server()
+    while True:
+        message = message_from_server(socket)
+        process_server_message(message, socket)
 
 
 if __name__ == '__main__':
--- modules/server.py
+++ modules/server.py
@@ -1,10 +1,17 @@
 """Server module manages connections to clients.
 
 Hard-coded to work with two clients, at fixed ports.
+
+start_server() starts the server sockets for each member of PORTS
+connect_client() connects each individual client, when they request to.
+message_to_client() sends a message to an individual client
+answer_from_client() reads back a message from an individual client
+send_table_to_client() packs and sends the table to the client for rendering
 """
 
 
 import socket
+import jsonpickle
 
 PORTS = [8000, 8001]
 
@@ -50,6 +57,12 @@ def answer_from_client(clientsocket):
     """
     ans_string = clientsocket.recv(1024).decode()
     return ans_string
+
+
+def send_table_to_client(table, clientsocket):
+    """Sends the table to the client for rendering."""
+    table_json = jsonpickle.encode(table)
+    clientsocket.send(table_json.encode())
 
 
 def main():
