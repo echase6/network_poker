c9446e9bef12ed2c0ccd170b2dd139d0d73a465b
Stephen <stevemuller@gmail.com>
2016-08-02 10:29:03 -0700

    improved deck docstrings

--- deck.py
+++ deck.py
@@ -1,4 +1,9 @@
-""""Sets up the Deck class for a network Poker game"""
+""""The Deck class for the network poker game, stores a deck of cards as a list of 52 card instances.
+
+generate_deck() creates an instance of Deck with the 52 standard playing cards.
+shuffle_the_deck() randomizes the order of the deck in place.
+draw_card_from_deck() pulls the last card in the list of cards.
+"""
 
 
 from card import Card
@@ -6,9 +11,9 @@ from random import shuffle
 
 
 class Deck:
+    """Deck Class"""
     def __init__(self, cards):
         self.cards = cards
-
 
     def __eq__(self, other):
         """Return eq
@@ -27,9 +32,9 @@ class Deck:
             self.cards == other.cards
         )
 
-
     def __repr__(self):
         """Return repr.
+
         >>> repr(Deck([Card('H', '5'), Card('C', '2')]))
         "Deck([Card('H', '5'), Card('C', '2')])"
         """
@@ -39,7 +44,8 @@ class Deck:
 
 
 def generate_deck(suits, ranks):
-    """create a list of Cards and save them to a Deck
+    """Creates a list of all 52 cards and places them into an instance of Deck.
+
     >>> generate_deck(['H', 'D'], ['1', '2', '3'])
     Deck([Card('H', '1'), Card('H', '2'), Card('H', '3'), Card('D', '1'), Card('D', '2'), Card('D', '3')])
     """
@@ -51,10 +57,11 @@ def generate_deck(suits, ranks):
 
 
 def shuffle_the_deck(deck):
-    """relies on the random library shuffle function to reliably randomize the order of cards, does this in place"""
+    """Uses the shuffle function from the random library to shuffle the list of cards in the Deck instance."""
     shuffle(deck.cards)
 
 
 def draw_card_from_deck(deck):
-    """Removes a card from the deck to add to the players hand"""
+    """Uses the pop function to remove a card from the deck and returns it. The pop function pulls from the end of the
+    list of cards contained in a Deck instance."""
     return deck.cards.pop()
